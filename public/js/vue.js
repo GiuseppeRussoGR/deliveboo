bc28b89af945c9031269dd4cc605bfef55f2d062
DaniAlessi <danialessi87@gmail.com>
2021-07-13 17:29:00 +0200

    giuseppe

--- public/js/vue.js
+++ public/js/vue.js
@@ -2265,7 +2265,7 @@ module.exports = g;
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\girav\Desktop\Boolean Progetto\deliveboo\resources\js\vue.js */"./resources/js/vue.js");
+module.exports = __webpack_require__(/*! C:\Users\danie\Downloads\Progetto finale\deliveboo\resources\js\vue.js */"./resources/js/vue.js");
 
 
 /***/ })
