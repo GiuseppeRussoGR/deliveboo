886d50890fc25967d914d14ac7010a325062526c
endritmorina94 <endrit.morina@outlook.it>
2021-07-19 01:03:35 +0200

    migliorato struttura html, e grafica generale

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,96 +43,94 @@
 
 
     <!-- Inizio Navigator -->
-    <nav>
-        <div class="logo">
+    <nav class="row">
+        <div class="logo col-3">
             <h5>Delive<span>Boo</span></h5>
         </div>
-        <div class="login">
-            <a href="{{ route('register') }}">
-        <span class="subtext">
-            Sei un ristoratore? Collabora con noi.
-        </span>
-                <i class="fas fa-user-circle icon"></i>
-            </a>
+        <div class="col-9">
+            <div class="login">
+                <a class="float-right" href="{{ route('register') }}">
+                    <span class="subtext">
+                        Sei un ristoratore? Collabora con noi.
+                    </span>
+                    <i class="fas fa-user-circle icon"></i>
+                </a>
+            </div>
         </div>
     </nav>
     <!-- Fine Navigator -->
 
     <!-- Inizio Jumbotron -->
-    <div class="container my-jumbotron" :class="{hide : categoryChosen}">
-        <div class="row">
-            <div class="col-7 jumbotron-text">
-                <h2>Ordina cibo della tua zona con l'app</h2>
+    <div class="my-jumbotron row" :class="{hide : categoryChosen}">
+        <div class="col-7 jumbotron-text">
+            <h2>Ordina cibo della tua zona con l'app</h2>
 
-                <div class="subtitle">
-                    Scegli una delle categorie e visualizza subito i menù di tutti i ristoranti disponibili
-                </div>
+            <div class="subtitle">
+                Scegli una delle categorie e visualizza subito i menù di tutti i ristoranti disponibili
             </div>
-            {{-- fine col-left  --}}
-
-            {{-- col-right  --}}
-            <div class="col-5 banner-image">
-                <img
-                    src="https://st.depositphotos.com/1008939/1376/i/950/depositphotos_13766635-stock-photo-eating-pizza.jpg"
-                    alt="">
-            </div>
-            {{-- fine col-right  --}}
         </div>
+           
+        <div class="col-5 banner-image">
+            <img src="https://st.depositphotos.com/1008939/1376/i/950/depositphotos_13766635-stock-photo-eating-pizza.jpg"
+                alt="">
+        </div>
+            
     </div>
     <!-- Fine Jumbotron -->
 
     <!-- Inizio Tipologie -->
-    <div class="types-section" :class="{hide : restaurantChosen}">
-        <h6>
+    <div class="types-section row" :class="{hide : restaurantChosen}">
+        <h6 class="col-12">
             Esplora le Tipologie
         </h6>
 
-        <div class="types-container">
-            {{-- card  --}}
-            <div class="type-card" v-for="type in types"
-                 @click='getApi("api/restaurants/", "restaurants", type.id); categoryChosen = true'>
-                <div class="img-container">
-                    <i class="fas fa-pizza-slice"></i>
-                </div>
-                <div class="card-title">
-                    @{{type.name}}
-                </div>
-                <div class="card-icon">
-                    <i class="fas fa-chevron-right"></i>
+        <div class="container">
+            <div class="type-cards">
+                <div class="" v-for="type in types">
+                    <div class="type-card" @click='getApi("api/restaurants/", "restaurants", type.id); categoryChosen = true'>
+                        <div class="img-container">
+                            <i class="fas fa-pizza-slice"></i>
+                        </div>
+                        <div class="card-title">
+                            @{{type.name}}
+                        </div>
+                        <div class="card-icon">
+                            <i class="fas fa-chevron-right"></i>
+                        </div>
+                    </div>
                 </div>
             </div>
-            {{-- end card  --}}
         </div>
-
-
     </div>
     <!-- Fine Tipologie -->
 
     <!-- Inizio Ristoranti -->
-    <div class="restaurants-container" :class="[{show : categoryChosen}, {hide : restaurantChosen}]">
-        <div class="restaurant-card" v-for='(restaurant, index) in restaurants'
-             @click="getApi('api/dishes/','dishes',restaurant.id); restaurantChosen = true; chosenRestaurantIndex = index">
-            <div class="restaurant-img">
-                <img :src="'storage/' + restaurant.img_path" alt="#">
-            </div>
+    <div class="restaurants-container row" :class="[{show : categoryChosen}, {hide : restaurantChosen}]">
+        <div class="col-6" v-for='(restaurant, index) in restaurants'> 
+            <div class="restaurant-card" 
+                @click="getApi('api/dishes/','dishes',restaurant.id); restaurantChosen = true; chosenRestaurantIndex = index">
+                <div class="restaurant-img">
+                    <img :src="'storage/' + restaurant.img_path" alt="#">
+                </div>
 
-            <div class="restaurant-infos">
-                <div class="card-title">
-                    @{{restaurant.name}}
-                </div>
-                <div class="restaurant-location">
-                    <div class="restaurant-address">
-                        <i class="fas fa-map-marker-alt"></i>
-                        @{{restaurant.address}}
+                <div class="restaurant-infos">
+                    <div class="card-title">
+                        @{{restaurant.name}}
                     </div>
-                    <div class="restaurant-city">
-                        @{{restaurant.city}}
+                    <div class="restaurant-location">
+                        <div class="restaurant-address">
+                            <i class="fas fa-map-marker-alt"></i>
+                            @{{restaurant.address}}
+                        </div>
+                        <div class="restaurant-city">
+                            @{{restaurant.city}}
+                        </div>
                     </div>
-                </div>
-                <div class="restaurant-rate subtext">
-                    <i class="fas fa-star"></i>
-                    Molto Buono
-                    <span>(88)</span>
+                    <div class="restaurant-rate subtext">
+                        <i class="fas fa-star"></i>
+                        Molto Buono
+                        <span>(88)</span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -142,70 +140,71 @@
     <div v-if="restaurantChosen" class="single-restaurant-page">
 
         <!-- Inizio Singolo Ristorante -->
-        <div class="single-restaurant-container">
+        <div class="single-restaurant row">
+            <div class="img-container col-6">
+                <img :src="'storage/' + restaurants[chosenRestaurantIndex].img_path" alt="">
+            </div>
 
-            <div class="single-restaurant-card container">
-                <div class="row">
+            <div class="single-restaurant-info col-6">
+                        
+                <h5>@{{restaurants[chosenRestaurantIndex].name}}</h5>
+                    
 
-                    <div class="img-container col-6">
-                        <img :src="'storage/' + restaurants[chosenRestaurantIndex].img_path" alt="">
-                    </div>
-
-                    <div class="single-restaurant-info col-6">
-                        <div class="card-title">
-                            <h2> @{{restaurants[chosenRestaurantIndex].name}}</h2>
-                        </div>
-
-                        <div class="restaurant-location">
-
-                            <i class="fas fa-map-marker-alt"></i>
-                            <h5>@{{restaurants[chosenRestaurantIndex].address}}</h5>
-
-                        </div>
-
-                        <div class="restaurant-rate subtext">
-                            <i class="fas fa-star"></i>
-                            Molto Buono
-                            <span>(88)</span>
-                        </div>
-
-                        <div class="restaurant-category">
-                            <span>Categoria:</span>
-                        </div>
-
-                    </div>
+                <div class="restaurant-location">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <span class="card-title">
+                        @{{restaurants[chosenRestaurantIndex].address}}, @{{restaurants[chosenRestaurantIndex].city}}
+                    </span>
                 </div>
+
+                <div class="restaurant-rate">
+                    <i class="fas fa-star"></i>
+                    Molto Buono
+                    <span>(88)</span>
+                </div>
+
+                        <!-- <div class="restaurant-category">
+                            <span>Categoria:</span>
+                        </div> -->
 
             </div>
         </div>
         <!-- Fine Singolo Ristorante -->
 
-        <!-- Inizio Menu -->
-        <div class="menu">
+        <!-- Inizio Dishes -->
+        <div class="dishes row">
 
-            <div class="dish-card-container">
-                <div class="dish-card" :class="{'cart-open' : openBasket}"
-                     v-for="(dish, index) in dishes">
-                    <img :src="'storage/' + dish.img_path" alt=""
-                         @click="insertBasket(index,$('#quantity-'+ index ).val()); openBasket = true">
+            <div :class="openBasket ? 'col-4' : 'col-3'" v-for="(dish, index) in dishes">
+                <div class="dish-card" :class="{'cart-open' : openBasket}">
+                    <img :src="'storage/' + dish.img_path" :alt="dish.name">
                     <div class="dish-content">
-                        <span class="dish-name">@{{ dish.name }}</span>
-                        <span class="dish-description">@{{ dish.description }}</span>
-                        <div>
+                        <span class="dish-name card-title">
+                            @{{ dish.name }}
+                        </span>
+                        <span class="dish-description">
+                            @{{ dish.description }}
+                        </span>
+                        <div class="dish-controls">
                             <span class="price">@{{ dish.price }} €</span>
                             <!-- Ricorda di settare il più e il meno -->
-                            <span @click="setQuantity($('#quantity-'+ index ), '-')">-</span>
-                            <input min="1" :id="'quantity-' + index" type="number" name="quantita" value="1">
-                            <span @click="setQuantity($('#quantity-'+ index ), '+')">+</span>
-                            <div class="cart-button"><i class="fas fa-cart-plus"></i></div>
+                            <div class="number-input">
+                                <button @click="setQuantity($('#quantity-'+ index ), '-')"></button>
+                                <input min="1" :id="'quantity-' + index" type="number" name="quantita" value="1" class="quantity">
+                                <button @click="setQuantity($('#quantity-'+ index ), '+')" class="plus"></button> 
+                            </div>
+                            <div class="cart-button" @click="insertBasket(index,$('#quantity-'+ index ).val()); openBasket = true">
+                                <i class="fas fa-shopping-cart"></i>
+                                <div class="plus-icon">
+                                    <i class="fas fa-plus"></i>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
                 </div>
             </div>
 
         </div>
-        <!-- Fine Menu -->
+        <!-- Fine Dishes -->
     </div>
 @endsection
 
@@ -215,6 +214,9 @@
         <div class="title">
             <div class="subtitle">
                 Carrello
+            </div>
+            <div class="closing-icon">
+                <i class="fas fa-times"></i>
             </div>
         </div>
 
@@ -256,10 +258,6 @@
                 <label for="client_city">Città</label>
                 <input type="text" class="form-control" id="client_city" name="client_city" placeholder="Milano">
             </div>
-            <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#payment">
-                Manda l'ordine
-            </button>
-
         </form>
 
         <div class="cart-subtitle">
@@ -268,10 +266,9 @@
 
         <div class="order-items">
             <ul v-if="order.dishes.length > 0">
-                <li v-for="dish in  order.dishes ">
-                    <img src="https://static.gamberorosso.it/da-zero.jpg" alt="">
-                    <span>@{{ dish.nome }}</span>
-                    <span>@{{ dish.totale_singolo }} €</span>
+                <li v-for="dish in  order.dishes">
+                    <span class="dish-name">@{{ dish.nome }} <span class="dish-quantity">(x@{{dish.quantita}})</span></span>
+                    <span class="dish-total-price">@{{ dish.totale_singolo }} € <i class="fas fa-times"></i></span>
                 </li>
             </ul>
             <ul v-else>
@@ -281,16 +278,21 @@
             </ul>
         </div>
 
-        <div class="order-total">
-        <span class="total-order">
-            Totale Ordine:
-        </span>
-            <span>
-            @{{ order.total_price }} €
-        </span>
+        <div class="bottom-info">
+            <div class="order-total">
+                <span class="total-order">
+                    Totale Ordine:
+                </span>
+                <span>
+                @{{ order.total_price }} €
+                </span>
+            </div>
+            <button class="btn-checkout" data-toggle="modal" data-target="#payment">
+                    CHECKOUT
+            </button>
         </div>
 
-        {{--Modal--}}
+        <!-- Inizio Modale -->
         <div class="modal fade" id="payment" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
              aria-hidden="true">
             <div class="modal-dialog" role="document">
@@ -311,7 +313,7 @@
                 </div>
             </div>
         </div>
-        {{--End Modal--}}
+        <!-- Fine Modale -->
     </div>
     <!-- Fine Carrello -->
 @endsection
