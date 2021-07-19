085ac2a48d24626c04e7b1223f7ea8c694b4e646
DaniAlessi <danialessi87@gmail.com>
2021-07-19 10:42:27 +0200

    aggiunte card home amministratore

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,18 +3,39 @@
 @section('app_style', 'user')
 
 @section('content')
-<a href=" {{route('admin.user.create')}} " class="btn btn-primary">Aggiungi nuovo piatto</a>
-<ul>
-@foreach($dishes as $dish)
-<li>{{$dish->name}}
-  <ul>
-    <li><img src="{{ asset('storage/' . $dish->img_path) }}" alt="Dish Cover"></li>
-    <li>{{$dish->description}}</li>
-    <li>{{$dish->price}}</li>
-    <li><a href="{{ route('admin.user.edit', [ 'user' => $dish->id ])}}" class="btn btn-success">Modifica</a></li>
-    <li><a href="{{ route('admin.user.show', [ 'user' => $dish->id ])}}" class="btn btn-primary">Dettagli</a></li>
-  </ul>
-</li>
-@endforeach
-</ul>
+<div class="container">
+  <a href=" {{route('admin.user.create')}} " class="btn btn-primary">Aggiungi nuovo piatto</a>
+  
+  <div class="row">
+
+    @foreach($dishes as $dish)
+    <div class="card" style="width: 18rem;">
+      <img src="{{ asset('storage/' . $dish->img_path) }}" class="card-img-top" alt="Dish Cover">
+      <div class="card-body">
+        <h5 class="card-title">{{$dish->name}}</h5>
+        <p class="card-text">{{$dish->description}}</p>
+        <h5 class="card-title">{{$dish->price}}</h5>
+        <a href="{{ route('admin.user.edit', [ 'user' => $dish->id ])}}" class="btn btn-success">Modifica</a>
+        <a href="{{ route('admin.user.show', [ 'user' => $dish->id ])}}" class="btn btn-primary">Dettagli</a>
+      </div>
+    </div>
+    @endforeach
+
+  </div>
+
+    <ul>
+   
+    <li>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </li>
+    
+    </ul>
+</div>
+
 @endsection
